698d4bcef193153c1023864774746efc84aca99b
sdfedele <shiny.dzack@gmail.com>
2023-07-08 18:44:00 -0600

    Begin quackening

--- Assets/_Gameplay/Player/PlayerStatus.cs
+++ Assets/_Gameplay/Player/PlayerStatus.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatus : MonoBehaviour
 {
     [SerializeField] UIAnimatedElement _uiAlert;
     [SerializeField] GameEventDogState _evDogStateChange;
+    [SerializeField] Slider _sQuack;
+    [SerializeField] float _durationQuackBuildup;
+    [SerializeField] Vector2 _quackBuildupVariance;
+    [SerializeField] InvestigateNode _prefabSoundNode;
+
     public Vector3 PlayerPosition => transform.position;
     public bool Immortal = false;
+
+    float _quackUrge;
 
     private void OnEnable()
     {
@@ -18,9 +26,38 @@ public class PlayerStatus : MonoBehaviour
         _evDogStateChange.Unsubscribe(CheckDogState);
     }
 
-    public void BecomeImmortal()
+    private void Awake()
+    {
+        _sQuack.value = 0f;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    IEnumerator QuackManagement()
+    {
+        while (!Immortal) {
+            float startTime = Time.time;
+            float quackSum = _durationQuackBuildup + Random.Range(_quackBuildupVariance.x, _quackBuildupVariance.y);
+            while (Time.time < startTime + quackSum) {
+                float t = (Time.time - startTime) / quackSum;
+                _sQuack.value = t;
+                yield return null;
+            }
+
+        }
+    }
+
+    void Quack()
     {
 
+    }
+
+    public void BecomeImmortal()
+    {
+        Immortal = true;
     }
 
     void CheckDogState(DogState state)
